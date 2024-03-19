759e5d0f4888be81d99db71f2e136b5658ea5dd2
Enrique Gomez Naar <enriquegomeznaar@gmail.com>
2024-03-19 12:48:49 -0300

    list of persons in order

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HolaMundoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface Persona {
+  nombre: string;
+  apellido: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,5 +12,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  personas: Persona[] = [];
+  @ViewChild('personaForm') personaForm?: NgForm;
 
+  agregarPersona() {
+    if (this.personaForm?.valid) {
+      const nombre = this.personaForm.value.nombre.charAt(0).toUpperCase() + this.personaForm.value.nombre.slice(1);
+      const apellido = this.personaForm.value.apellido.charAt(0).toUpperCase() + this.personaForm.value.apellido.slice(1);
+      this.personas.push({ nombre, apellido });
+      this.personas.sort((a, b) => a.apellido.localeCompare(b.apellido)); 
+      this.personaForm.resetForm();
+    }
+  }
 }
