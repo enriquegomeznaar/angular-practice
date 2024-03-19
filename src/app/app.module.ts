10672eebc29c1f32c6c014dcc70f865a5703d9bc
Enrique Gomez Naar <enriquegomeznaar@gmail.com>
2024-03-19 12:52:55 -0300

    list of persons in order

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HolaMundoComponent
   ],
   imports: [
     BrowserModule,
